497e9939a85f09b02f8ea246de7cb718da879210
Jihong Park <ggikko@hotmail.com>
2016-09-30 20:51:16 +0900

    init boyermoore

---
+++ src/me/ggikko/boyermoore/BoyerMoore.java
@@ -0,0 +1,11 @@
+package me.ggikko.boyermoore;
+
+/**
+ * Created by admin on 2016. 9. 30..
+ */
+public class BoyerMoore {
+
+    public static void main(String[] args) {
+        System.out.printf("boyerMoore");
+    }
+}
