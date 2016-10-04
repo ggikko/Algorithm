33ae7de672fb4bf32b57d65ec4b410e3d5b6a856
Jihong Park <ggikko@hotmail.com>
2016-10-04 23:21:24 +0900

    add comment for sparse array

--- src/me/ggikko/datastructure/SparseArray.java
+++ src/me/ggikko/datastructure/SparseArray.java
@@ -9,23 +9,35 @@ public class SparseArray {
     private List start;
     private int index;
 
+    /**
+     * create list & set index
+     * @param index
+     */
     SparseArray(int index) {
         start = new List();
         this.index = index;
     }
 
+    /**
+     * add object
+     * @param index
+     * @param value
+     */
     public void store(int index, Object value) {
         if (index >= 0 && index < this.index) {
-            if (value != null)
-                start.store(index, value);
+            if (value != null) start.store(index, value);
         } else {
             System.out.println("INDEX OUT OF BOUNDS");
         }
     }
 
+    /**
+     * fetch object
+     * @param index
+     * @return
+     */
     public Object fetch(int index) {
-        if (index >= 0 && index < this.index)
-            return start.fetch(index);
+        if (index >= 0 && index < this.index) return start.fetch(index);
         else {
             System.out.println("INDEX OUT OF BOUNDS");
             return null;
@@ -36,6 +48,9 @@ public class SparseArray {
         return start.elementCount();
     }
 
+    /**
+     * List
+     */
     class List {
         private int index;
         private Object value;
@@ -102,25 +117,23 @@ public class SparseArray {
     }
 
     public static void main(String[] args) {
+
+        //TODO : android 에서 sparse array는 int형만 받음. 다시 보기.
         Integer[] array = {1, null, 2, 3, null};
 
         int size = array.length;
 
         SparseArray sparseArray = new SparseArray(5);
-        for (int i = 0; i < size; i++) {
-            sparseArray.store(i, array[i]);
-        }
+        for (int i = 0; i < size; i++) sparseArray.store(i, array[i]);
+
 
         System.out.println("NORMAL ARRAY");
-        for (int i = 0; i < size; i++) {
-            System.out.print(array[i] + "\t");
-        }
+        for (int i = 0; i < size; i++) System.out.print(array[i] + "\t");
+
 
         System.out.println("\nSPARSE ARRAY");
-        for (int i = 0; i < size; i++) {
-            if (sparseArray.fetch(i) != null)
-                System.out.print(sparseArray.fetch(i) + "\t");
-        }
+        for (int i = 0; i < size; i++) if (sparseArray.fetch(i) != null) System.out.print(sparseArray.fetch(i) + "\t");
+
         System.out.println("The Size of Sparse Array is " + sparseArray.elementCount());
     }
 }
